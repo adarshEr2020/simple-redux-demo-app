97298f00ef58865029366567930a3357c8e86312
adarsh prajapati <adarshprajapati31@gmail.com>
2023-02-09 19:48:48 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+// components
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+// items
+import cartItems from "./cart-items";
+import { createStore } from "redux";
+import reducer from "./reducer";
+
+// dispatch method - send actions to the store
+// actions(objects) - MUST HAVE TYPE PROPERTY- what kind of action
+// DON'T MU"TATE THE STATE - redux built on IMMUTABILITY(copy)
+
+//// redux stuff
+
+// reducer -  function that used to update store
+// two arguments - state, action
+// State - old state/state before update
+// action -  what happened/ what update
+// return updated or old state
+
+// store.getState()-
+
+import { Provider } from "react-redux";
+// initial store
+const initialStore = {
+  cart: cartItems,
+  total: 105,
+  amount: 5,
+};
+
+// store
+
+const store = createStore(
+  reducer,
+  initialStore,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+function App() {
+  // cart setup
+  return (
+    <Provider store={store}>
+      <Navbar />
+      <CartContainer />
+    </Provider>
+  );
+}
+
+export default App;
---
+++ src/cart-items.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    title: "Samsung Galaxy S7",
+    price: 599.99,
+    img:
+      "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-s7--.jpg",
+    amount: 1
+  },
+  {
+    id: 2,
+    title: "google pixel ",
+    price: 499.99,
+    img:
+      "https://fdn2.gsmarena.com/vv/pics/google/google-pixel-02.jpg",
+    amount: 1
+  },
+  {
+    id: 3,
+    title: "Xiaomi Redmi Note 2",
+    price: 699.99,
+    img:
+      "https://fdn2.gsmarena.com/vv/bigpic/xiaomi-redmi-note-2--.jpg",
+    amount: 1
+  }
+];
